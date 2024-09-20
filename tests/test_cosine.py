4f888ab72ac4f96ac32381deda74264340be8808
Lokesh Veeramacheneni <veeramacheneni.lokesh@proton.me>
2024-09-20 10:14:31 +0200

    Update test_cosine.py

--- tests/test_cosine.py
+++ tests/test_cosine.py
@@ -2,9 +2,8 @@
 
 import sys
 
-import jax
-import jax.numpy as jnp
 import pytest
+import torch as th
 
 sys.path.insert(0, "./src/")
 
@@ -14,42 +13,42 @@ from src.denoise_cosine import cost, net, sigmoid
 @pytest.mark.parametrize("seed", [0, 1, 2, 3, 42])
 def test_sigmoid(seed) -> None:
     """See if the sigmoid is correct and really returns true."""
-    test_sig = lambda x: jnp.exp(x) / (1.0 + jnp.exp(x))  # noqa: E731
-    key = jax.random.PRNGKey(seed)
-    x = jax.random.uniform(key, [1])
-    assert jnp.allclose(sigmoid(x), test_sig(x))
+    test_sig = lambda x: th.exp(x) / (1.0 + th.exp(x))  # noqa: E731
+    th.manual_seed(seed)
+    x = th.rand([1])
+    assert th.allclose(sigmoid(x), test_sig(x))
 
 
 testdata = [
-    (jnp.array([1, 2, 3]), jnp.array([4, 5, 6]), jnp.array([13.5])),
-    (jnp.array([42, 21, 11]), jnp.array([4, 5, 3]), jnp.array([882.0])),
+    (th.tensor([1, 2, 3]), th.tensor([4, 5, 6]), th.tensor([13.5])),
+    (th.tensor([42, 21, 11]), th.tensor([4, 5, 3]), th.tensor([882.0])),
 ]
 
 
 @pytest.mark.parametrize("y, h, res", testdata)
 def test_mse(y, h, res) -> None:
-    """See it the cost is correct for two samples and returns an array."""
-    assert jnp.allclose(cost(y, h), res)
+    """See if the cost is correct for two samples and returns an tensor."""
+    assert th.allclose(cost(y, h), res)
 
 
 test_net_data = [
     (
         {
-            "W1": jnp.array([[3, 2], [1, 2]]),
-            "W2": jnp.array([[3, 4], [5, 6]]),
-            "b": jnp.array([42]),
+            "W1": th.tensor([[3, 2], [1, 2]]).type(th.float64),
+            "W2": th.tensor([[3, 4], [5, 6]]).type(th.float64),
+            "b": th.tensor([42]).type(th.float64),
         },
-        jnp.array([13, 12]),
-        jnp.array([7.0, 11.0]),
+        th.tensor([13.0, 12.0]).type(th.float64),
+        th.tensor([7.0, 11.0]).type(th.float64),
     ),
     (
         {
-            "W1": jnp.array([[3, 42], [1, 2]]),
-            "W2": jnp.array([[3, 4], [23, 6]]),
-            "b": jnp.array([21]),
+            "W1": th.tensor([[3, 42], [1, 2]]).type(th.float64),
+            "W2": th.tensor([[3, 4], [23, 6]]).type(th.float64),
+            "b": th.tensor([21]).type(th.float64),
         },
-        jnp.array([13, 12]),
-        jnp.array([7.0, 29.0]),
+        th.tensor([13.0, 12.0]).type(th.float64),
+        th.tensor([7.0, 29.0]).type(th.float64),
     ),
 ]
 
@@ -57,5 +56,5 @@ test_net_data = [
 @pytest.mark.parametrize("params, x, res", test_net_data)
 def test_net(params, x, res) -> None:
     """See it the net is correct for two samples and returns an array."""
-    print(net(params, x))
-    assert jnp.allclose(net(params, x), res)
+    output = net(params, x)
+    assert th.allclose(output, res)
